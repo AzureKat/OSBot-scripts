3e456e62234fa6ade96b61eb88921a9d728f1a6a
aleksivas <a1503060@myy.haaga-helia.fi>
2017-03-06 16:18:32 +0200

    Update scriptname
    this shit

--- src/ScriptName.java
+++ src/ScriptName.java
@@ -1,3 +1,6 @@
+import org.osbot.Lo;
+import org.osbot.rs07.api.model.Entity;
+import org.osbot.rs07.api.model.Interactable;
 import org.osbot.rs07.api.ui.Message;
 import org.osbot.rs07.script.MethodProvider;
 import org.osbot.rs07.script.Script;
@@ -8,11 +11,9 @@ import java.awt.*;
 @ScriptManifest(name = "pip", author = "lul", version = 1.0, info = "", logo = "")
 public final class ScriptName extends Script {
 
-    @Override
-    public final int onLoop() throws InterruptedException {
 
-        testClass testclass = new testClass(this);
-        return 0;
+    private enum State {
+        CHOP, DROP, WAIT
     }
 
     @Override
@@ -20,33 +21,60 @@ public final class ScriptName extends Script {
         log("This is a test script");
     }
 
+
+    private State getState(){
+        Entity check = objects.closest("Oak Tree");
+
+        if(inventory.isFull()){
+            return State.DROP;
+        }
+
+        if(check != null){
+            return State.CHOP;
+        }
+
+        return State.WAIT;
+
+    }
+
+    @Override
+    public final int onLoop() throws InterruptedException {
+
+        Interactable tree = null;
+        tree = objects.closest("Oak Tree");
+        int Log = 3511;
+
+        switch(getState()) {
+            case CHOP:
+                tree = objects.closest("Oak Tree");
+                if(tree != null){
+                    tree.interact("Chop Down");
+                }
+                sleep(random(1000));
+                break;
+
+            case DROP:
+                if(inventory.isFull()){
+                    inventory.drop(Log);
+                }
+                break;
+
+            case WAIT:
+                sleep(random(3000, 7000));
+                break;
+        }
+        return 0;
+    }
+
+
+
     @Override
     public final void onExit() {
         log("Script exiting");
     }
 
-    @Override
-    public final void onMessage(final Message message) {
-        log("A message has appeared" + message.getMessage());
-    }
-
-    @Override
-    public void onPaint(final Graphics2D g) {
-        g.drawString("Some text", 10, 10);
-    }
 
 
-    final class testClass {
 
-        private final MethodProvider methods;
 
-        private testClass(final MethodProvider methods) {
-            this.methods = methods;
-        }
-
-        public final void someMethod(){
-            methods.getInventory();
-        }
-
-    }
 }
