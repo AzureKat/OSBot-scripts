fa8158bc7f6528f4ce839b7034a853519d9beb45
aleksivas <a1503060@myy.haaga-helia.fi>
2017-07-05 17:00:51 +0300

    Update Fletcher.java

--- src/Fletcher.java
+++ src/Fletcher.java
@@ -1,78 +1,69 @@
 import org.osbot.rs07.api.model.Entity;
-import org.osbot.rs07.api.model.Interactable;
-import org.osbot.rs07.api.model.RS2Object;
+import org.osbot.rs07.api.model.NPC;
 import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
 
-public final class Fletcher extends Script {
+@ScriptManifest(author = "x7599", info = "fish n drop", name = "PowerFisher", logo = "", version = 1.0)
+public final class Fisher extends Script {
 
     private enum State {
-        BANK, FLETCH, WAIT, WITHDRAW
+        FISH, DROP, WAIT
     }
 
     @Override
-    public final void onStart() {
-        log("Fletcher");
+    public final void onStart(){
+        log("some good shit");
     }
 
     private State getState(){
 
-        int bowId = 0;
-        int bowString = 0;
-        int completedbow = 0;
+        int feather = 314;
+        int rod = 309;
 
-        if (getInventory().isEmpty() || getInventory().contains(completedbow)){
-            return State.BANK;
+        if(getInventory().contains(feather, rod) && !getInventory().contains("Raw trout", "Raw salmon")){
+            return State.FISH;
         }
-
-        if(getBank().isOpen()){
-            return State.WITHDRAW;
+        if(getInventory().isFull()){
+            return State.DROP;
         }
-
-        if(getInventory().contains(bowId, bowString)){
-            return State.FLETCH;
-        }
-
         return State.WAIT;
-
     }
 
     public final int onLoop() throws InterruptedException {
 
-        int bowId = 0;
-        int bowString = 0;
+
+        int feather = 314;
+        int rod = 309;
 
         switch (getState()){
-            case BANK:
-                Entity bank = getObjects().closest("Bank booth");
-                if(bank != null && bank.exists()){
-                if(bank.interact("Bank")){
-                    new Sleep(() -> getBank().isOpen(), 5000).sleep();
+            case FISH:
+                NPC fishSpot = this.getNpcs().closest("Fishing spot");
+                if(fishSpot != null){
+                    fishSpot.interact("Lure");
+                    sleep(random(3000, 5000));
+                    Sleep.sleepUntil(() -> !myPlayer().isAnimating(), 5000);
+                    getMouse().moveOutsideScreen();
                 }
-            }
-            break;
-            case WITHDRAW:
-                if (getBank().isOpen()){
-                    if(!getInventory().contains(bowId, bowString)){
-                        if (getBank().contains(bowId, bowString)) {
-                            getBank().withdraw(bowId, 14);
-                            getBank().withdraw(bowString, 14);
-                            getBank().close();
-                        }
-                    }
-                }
-            break;
-            case FLETCH:
-                if(getInventory().contains(bowId, bowString)){
-                    getInventory().interact("Use", bowId, bowString);
+                break;
 
-
+            case DROP:
+                if(getInventory().isFull()){
+                    sleep(random(200, 500));
+                    getInventory().dropAllExcept(feather, rod);
+                    sleep(random(300, 700));
                 }
+                break;
+
+            case WAIT:
+                sleep(random(3000, 7000));
         }
         return random(200, 300);
     }
 
+
     @Override
-    public final void onExit() {
-        log("Script exiting");
+    public final void onExit(){
+        log("Script dying..");
     }
+
 }
